8d950f3694c85bbb0450da42d8c8f599a2c100c6
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-05-15 14:59:34 -0300

    feat: blog page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { LandingPage } from "@/templates/landingpage"
-// import { allPosts } from "contentlayer/generated"
 
 export default function Home() {
-  // console.debug(allPosts)
   return <LandingPage />
 }
