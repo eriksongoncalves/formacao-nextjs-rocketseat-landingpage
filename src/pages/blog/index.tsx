4d3f3e3316abaa21e3f976ab97b6670121e9c4cc
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-05-17 14:06:01 -0300

    feat: add ssg in blog list

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,20 @@
-import { BlogList } from "@/templates/blog"
+import { GetStaticProps } from "next"
+import { allPosts } from "contentlayer/generated"
 
-export default function BlogPage() {
-  return <BlogList />
+import { BlogList, type BlogListProps } from "@/templates/blog"
+
+export default function BlogPage({ posts }: BlogListProps) {
+  return <BlogList posts={posts} />
 }
+
+export const getStaticProps = (async () => {
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+
+  return {
+    props: {
+      posts: sortedPosts
+    }
+  }
+}) satisfies GetStaticProps<BlogListProps>
